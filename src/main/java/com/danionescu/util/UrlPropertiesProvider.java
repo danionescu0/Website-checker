014375aeb9e039111ca282f7451fb80f6322eaf7
Dan Ionescu <ionescu.dan84@gmail.com>
2016-10-25 22:49:13 +0300

    renamed file

--- src/main/java/com/danionescu/util/UrlPropertiesProvider.java
+++ src/main/java/com/danionescu/util/UrlPropertiesProvider.java
@@ -1,5 +1,6 @@
 package com.danionescu.util;
 
+import com.danionescu.model.UrlProperties;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -9,11 +10,11 @@ import java.util.ArrayList;
 import java.util.stream.Stream;
 
 @Service
-public class UrlProvider {
-    public ArrayList<String> get(String filePath) {
-        ArrayList<String> list = new ArrayList<>();
+public class UrlPropertiesProvider {
+    public ArrayList<UrlProperties> get(String filePath) {
+        ArrayList<UrlProperties> list = new ArrayList<>();
         try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-            stream.forEach(line -> list.add(line));
+            stream.forEach(line -> list.add(new UrlProperties(line, 1000)));
         } catch (IOException e) {
             throw new RuntimeException("Could not read input file");
         }
